653f217bcd38090a17e571828ee733ee45bfed14
DoVanDung <dungtob2001@gmail.com>
2025-04-16 18:11:20 +0700

    bt27fix

--- bt27/post.js
+++ bt27/post.js
@@ -3,15 +3,16 @@ import { getPost,newToken } from "./api.js";
 const text = document.querySelector('h1')
 
 const onMounted =async() => {
+    if(data.detail == 'token expired'){
+        const fix = await newToken()
+     
+        localStorage.setItem("access", fix.access);
+        localStorage.setItem("refresh", fix.refresh);
+        await getPost()
+       }
    let data = await getPost()
    text.innerHTML += data
    console.log(data)
-   if(data.detail == 'token expired'){
-    const fix = await newToken()
- 
-    localStorage.setItem("access", fix.access);
-    localStorage.setItem("refresh", fix.refresh);
-    await getPost()
-   }
+   
 }
 onMounted()
